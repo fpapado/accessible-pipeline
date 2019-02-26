1d6abb427014b5c343ee2407b999824ca5c838fd
fpapado <fotis@futurice.com>
2019-02-26 23:11:02 +0200

    Experiment with links and Sets

--- index.ts
+++ index.ts
@@ -4,28 +4,39 @@ import {AxeResults} from 'axe-core';
 import cheerio from 'cheerio';
 import fs from 'fs';
 import {promisify} from 'util';
-
 const writeFile = promisify(fs.writeFile);
 
-const ENTRY = new URL('https://fiba3x3.com/');
+// Common options for excluding links to visit
+// For example *.pdf and #heading-link came up often in early iterations
+type Options = {
+  ignoreFragmentLinks?: boolean;
+  ignoreExtensions?: Array<string>;
+};
+
+const ENTRY = 'https://fiba3x3.com/';
 
 async function main() {
-  // TODO: Consider whether to store string or URL in the Set
   const PAGES_VISITED = new Set<string>();
-  let PAGES_TO_VISIT: Array<URL> = [ENTRY];
+  // It is preferable to store a string in the set, otherwise identities get jumbled with objects...
+  // Wish there was a good hashed set implementation
+  // To compensate, we store the string, and transform to/from URL href at the edges
+  let PAGES_TO_VISIT = new Set([ENTRY]);
   let RESULTS: Array<AxeResults> = [];
-  const PAGE_LIMIT = 5;
+  const PAGE_LIMIT = 20;
 
   const browser = await puppeteer.launch();
   let run = 0;
 
   // TODO: Maybe we need a while here
   // TODO: Consider Depth-First Search vs. Breadth-First Search
-  for (const pageUrl of PAGES_TO_VISIT) {
+  for (const pageHref of PAGES_TO_VISIT) {
     // Double check that the page has not been visited (might come into play for concurrency)
-    if (run < PAGE_LIMIT && !PAGES_VISITED.has(pageUrl.href)) {
+    if (run < PAGE_LIMIT && !PAGES_VISITED.has(pageHref)) {
       run++;
       console.log('Run', run);
+
+      // First, convert to a URL (if we have gotten this from the scripts below, this is OK)
+      const pageUrl = new URL(pageHref);
 
       console.log('Will check', pageUrl.href);
       // Add to pages visited
@@ -36,11 +47,13 @@ async function main() {
       console.log('Got results');
 
       // Append the results to the list
-      RESULTS = RESULTS.concat(results);
+      // RESULTS = RESULTS.concat(results);
 
       // Remove any visited pages, and add the rest to the ones to visit
       const pagesToVisit = getPagesToVisit(PAGES_VISITED, nextPages);
-      PAGES_TO_VISIT.push(...pagesToVisit);
+
+      // NOTE: We trasnform from a URL to href for storage, see the reasons in Set above
+      pagesToVisit.forEach(page => PAGES_TO_VISIT.add(page.href));
 
       // TODO: Format the results here somehow?
       // Alternative, after everything is gathered, run a reportResults(results)
@@ -48,13 +61,16 @@ async function main() {
       // console.log('Next pages', pagesToVisit);
     }
   }
+
+  // Close the browser and write reports
   await browser.close();
+
   await writeFile('report.json', JSON.stringify(RESULTS, null, 2), 'utf8');
   console.log('Wrote report.json');
 
   await writeFile(
     'queue.json',
-    JSON.stringify(PAGES_TO_VISIT.map(url => url.href), null, 2),
+    JSON.stringify(Array.from(PAGES_TO_VISIT.values()), null, 2),
     'utf8'
   );
   console.log('Wrote queue.json for remaining urls');
@@ -67,7 +83,8 @@ async function processPage(browser: Browser, pageUrl: URL) {
   await page.goto(pageUrl.href);
 
   // Analyse page, get results
-  const results = await analysePage(page);
+  const results: any[] = [];
+  // const results = await analysePage(page);
 
   // Gather next links
   // TODO: Consider not de-duplicating here, and returning all to the parent to decide
