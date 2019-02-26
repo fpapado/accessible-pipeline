fdfd3e9a3f514bf08b885b6c6268c4679ff88aaa
fpapado <fotis@futurice.com>
2019-02-26 23:35:51 +0200

    Love writing TODOs

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import puppeteer, {Browser, Page} from 'puppeteer';
 import {AxeResults} from 'axe-core';
 import cheerio from 'cheerio';
 import fs from 'fs';
+import path from 'path';
 import {promisify} from 'util';
 const writeFile = promisify(fs.writeFile);
 
@@ -13,9 +14,27 @@ type Options = {
   ignoreExtensions?: Array<string>;
 };
 
+const DEFAULT_OPTIONS: Partial<Options> = {};
+
+const TEST_OPTIONS = {
+  // TODO: Shoold ignoreExtensions allow not having the '.'?
+  ignoreExtensions: ['.pdf'],
+  // TODO: Should we have a way to say "include at least one fragment link"?
+  //  I cannot easily imagine a page having only 'example.com#thing' links to it, but it could happen...
+  //  or do we just advise folks to add it to roots?
+
+  // TODO: Consider the naming fragment vs hash
+  //  in particular, this is about 'example.com/faq#question and *not* about example.com/#/route
+  //  the former is common for linking to headings, the latter is common for some client-side routing
+  //  We should specify that we mean the former!
+  ignoreFragmentLinks: true,
+};
+
 const ENTRY = 'https://fiba3x3.com/';
 
-async function main() {
+async function main(opts?: Options) {
+  const options = {...DEFAULT_OPTIONS, ...opts};
+
   const PAGES_VISITED = new Set<string>();
   // It is preferable to store a string in the set, otherwise identities get jumbled with objects...
   // Wish there was a good hashed set implementation
@@ -50,7 +69,7 @@ async function main() {
       // RESULTS = RESULTS.concat(results);
 
       // Remove any visited pages, and add the rest to the ones to visit
-      const pagesToVisit = getPagesToVisit(PAGES_VISITED, nextPages);
+      const pagesToVisit = getPagesToVisit(nextPages, PAGES_VISITED, options);
 
       // NOTE: We trasnform from a URL to href for storage, see the reasons in Set above
       pagesToVisit.forEach(page => PAGES_TO_VISIT.add(page.href));
@@ -118,14 +137,36 @@ async function gatherNextPages(currentPageUrl: URL, pageObj: Page) {
   return relevantUrls;
 }
 
-function getPagesToVisit(visited: Set<string>, urls: Array<URL>) {
+function getPagesToVisit(
+  urls: Array<URL>,
+  visited: Set<string>,
+  options: Options
+) {
   // TODO: We could some fancy things here, such as:
   //  - checking a route matching
   //  - having a maximumDepth counter
   //  - some other metric / heuristic?
 
   // For now, follow verbatim
-  return urls.filter(url => !visited.has(url.href));
+  return urls
+    .filter(url => !visited.has(url.href))
+    .filter(url => {
+      if (options.ignoreExtensions) {
+        const ext = path.extname(url.pathname);
+        // Reject if extension is in ignore list
+        return !options.ignoreExtensions.includes(ext);
+      }
+      // Pass through otherwise
+      return true;
+    })
+    .filter(url => {
+      if (options.ignoreFragmentLinks) {
+        // Reject if there is a fragment
+        return !url.hash;
+      }
+      // Pass through otherwise
+      return true;
+    });
 }
 
-main();
+main(TEST_OPTIONS);
