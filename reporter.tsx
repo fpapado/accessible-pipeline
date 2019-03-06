0bf1c69d89a6bcf12f5ff150fd48be387bc98545
fpapado <fotis@futurice.com>
2019-03-06 22:04:42 +0200

    Add basic reporter

--- reporter.tsx
+++ reporter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, Box} from 'ink';
+import {render, Box, Text, Color} from 'ink';
 import fs from 'fs';
 import {promisify} from 'util';
 import {AxeResults} from 'axe-core';
@@ -9,9 +9,39 @@ const readFile = promisify(fs.readFile);
 
 type ReporterProps = {reportData: AxeResults[]};
 
-const Reporter: React.FunctionComponent<ReporterProps> = (props) => {
-  return <Box>Hello!</Box>;
-}
+const Reporter: React.FunctionComponent<ReporterProps> = props => {
+  const {reportData} = props;
+
+  return (
+    <Box flexDirection="column">
+      {reportData.map(result => (
+        <Box key={result.url} flexDirection="column" marginBottom={1}>
+          <Box>
+            <Text bold>{result.url}</Text>
+          </Box>
+          <Box>
+            <Color red>
+              <Text>Violations: </Text>
+              <Text>{result.violations.length}</Text>
+            </Color>
+          </Box>
+          <Box>
+            <Color green>
+              <Text>Passes: </Text>
+              <Text>{result.passes.length}</Text>
+            </Color>
+          </Box>
+          <Box>
+            <Color blue>
+              <Text>Incomplete: </Text>
+              <Text>{result.incomplete.length}</Text>
+            </Color>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+};
 
 // Main
 
@@ -24,4 +54,4 @@ async function main(reportFilename: string) {
   render(<Reporter reportData={reportData} />);
 }
 
-main('report.json');
+main('report-1551901587722.json');
